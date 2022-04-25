c98224aed64c846eb652b38269762b06bf66eed8
arvindsnowflake <104277429+arvindsnowflake@users.noreply.github.com>
2022-04-25 10:09:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ fruits_to_show = my_fruit_list.loc[fruits_seleced]
 streamlit.dataframe(my_fruit_list)
 
 
-streamlit.stop()
+
 
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
